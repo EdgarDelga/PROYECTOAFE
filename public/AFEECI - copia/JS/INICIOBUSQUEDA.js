783f2140d60e9534e8632720dcc30ab1aefb87c4
Edgar Delgadillo <95370665+EdgarDelga@users.noreply.github.com>
2022-08-26 16:06:48 -0500

    Add files via upload

---
+++ public/AFEECI - copia/JS/INICIOBUSQUEDA.js
@@ -0,0 +1,13 @@
+function mostrar (){
+    document.getElementById("datos2").style.display = "flex";
+}
+
+function ocultar(){
+    document.getElementById("datos").style.display = "none";
+    document.getElementById("datos2").style.display = "none";
+    document.getElementById("datos3").style.display = "none";
+}
+
+function mostrar1 (){
+    document.getElementById("datos3").style.display = "flex";
+}
